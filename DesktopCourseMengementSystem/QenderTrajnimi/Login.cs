02bc6d4506de7ddb305e158eea3f5658b66aceea
arbershaaa <aa29202@seeu.edu.mk>
2024-12-02 01:03:07 +0100

    C# Project

---
+++ DesktopCourseMengementSystem/QenderTrajnimi/Login.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QenderTrajnimi
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.\Arbresha\ARBRESHA;Initial Catalog=QenderTrajnimiDatabase;Integrated Security=True");
+            con.Open();
+            string select = "SELECT * From Useret Where username='" + this.textBox1.Text + "' AND password='" + this.textBox2.Text + "'";
+            SqlCommand cmd = new SqlCommand(select, con);
+            SqlDataReader reader = null;
+            reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                
+                MessageBox.Show("You have been logeed succesfully!");
+                textBox1.Text = textBox2.Text = "";
+                Form frm = new Home();
+                frm.Show();
+              
+                
+            }
+            else
+            {
+                MessageBox.Show("Username or Password is incorrect,please try again!");
+            }
+            con.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            label2.Text = "Please Enter the Username and Password to go to the \nAdmin page!";
+            label5.Text = "Forgot Password?";
+            label5.Font = new Font(label5.Font.Name, 8, FontStyle.Underline);
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.BorderSize = 0;
+            
+            
+           
+        }
+
+    
+
+           
+    }
+}
---
+++ DesktopCourseMengementSystem/QenderTrajnimi/Pagesat.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace QenderTrajnimi
+{
+    partial class Pagesat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pagesat));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnPagesatPerfunduara = new System.Windows.Forms.Button();
+            this.btnBorxhi = new System.Windows.Forms.Button();
+            this.btnPerfundo = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(546, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(99, 60);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefresh.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnRefresh.Location = new System.Drawing.Point(409, 114);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(115, 41);
+            this.btnRefresh.TabIndex = 23;
+            this.btnRefresh.Text = "Të Gjitha";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnPagesatPerfunduara
+            // 
+            this.btnPagesatPerfunduara.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPagesatPerfunduara.ForeColor = System.Drawing.Color.DarkGreen;
+            this.btnPagesatPerfunduara.Location = new System.Drawing.Point(167, 114);
+            this.btnPagesatPerfunduara.Name = "btnPagesatPerfunduara";
+            this.btnPagesatPerfunduara.Size = new System.Drawing.Size(115, 41);
+            this.btnPagesatPerfunduara.TabIndex = 22;
+            this.btnPagesatPerfunduara.Text = "Të përfunduara";
+            this.btnPagesatPerfunduara.UseVisualStyleBackColor = true;
+            this.btnPagesatPerfunduara.Click += new System.EventHandler(this.btnPagesatPerfunduara_Click);
+            // 
+            // btnBorxhi
+            // 
+            this.btnBorxhi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorxhi.ForeColor = System.Drawing.Color.Crimson;
+            this.btnBorxhi.Location = new System.Drawing.Point(288, 114);
+            this.btnBorxhi.Name = "btnBorxhi";
+            this.btnBorxhi.Size = new System.Drawing.Size(115, 41);
+            this.btnBorxhi.TabIndex = 21;
+            this.btnBorxhi.Text = "Borxhe";
+            this.btnBorxhi.UseVisualStyleBackColor = true;
+            this.btnBorxhi.Click += new System.EventHandler(this.btnBorxhi_Click);
+            // 
+            // btnPerfundo
+            // 
+            this.btnPerfundo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPerfundo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPerfundo.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.btnPerfundo.Location = new System.Drawing.Point(530, 114);
+            this.btnPerfundo.Name = "btnPerfundo";
+            this.btnPerfundo.Size = new System.Drawing.Size(115, 41);
+            this.btnPerfundo.TabIndex = 20;
+            this.btnPerfundo.Text = "Përfundo";
+            this.btnPerfundo.UseVisualStyleBackColor = true;
+            this.btnPerfundo.Click += new System.EventHandler(this.btnPerfundo_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label3.Location = new System.Drawing.Point(35, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 16);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Pagesat e regjistruara";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(38, 205);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(607, 230);
+            this.dataGridView1.TabIndex = 25;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(674, 24);
+            this.menuStrip1.TabIndex = 28;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.printToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(99, 22);
+            this.printToolStripMenuItem.Text = "Print";
+            this.printToolStripMenuItem.Click += new System.EventHandler(this.printToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(99, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // Pagesat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 458);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnPagesatPerfunduara);
+            this.Controls.Add(this.btnBorxhi);
+            this.Controls.Add(this.btnPerfundo);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Pagesat";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pagesat";
+            this.Load += new System.EventHandler(this.Pagesat_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnPagesatPerfunduara;
+        private System.Windows.Forms.Button btnBorxhi;
+        private System.Windows.Forms.Button btnPerfundo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+    }
+}
